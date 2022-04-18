12f22f0db168325844681ce78e77b5ef2c99010b
Luis Jonathan Carlosama <jonas_cj10k@outlook.com>
2022-04-18 11:54:15 -0500

    correcciones archivo static

--- app_webLecturas/settings.py
+++ app_webLecturas/settings.py
@@ -151,8 +151,9 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/fomentoLectura/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
