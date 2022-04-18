55b24fc60cbc9e63ea4321dd307a9e2e2518e0f4
Luis Jonathan Carlosama <jonas_cj10k@outlook.com>
2022-04-18 14:23:44 -0500

    actualizacion

--- app_webLecturas/settings.py
+++ app_webLecturas/settings.py
@@ -152,13 +152,16 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=(os.path.join(BASE_DIR, '/fomentoLectura/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
 MEDIA_URL = '/media/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'media'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL ='home'
+
 
 
 #configuracion email
